4378975c9fdfb796c1b150919be6e989327a30c9
Dhaval Mehta <dhaval.mehta@eliteinfoworld.com>
2017-11-02 17:26:10 +0530

    Initial commit

---
+++ FirebaseAll/app/src/main/java/com/auth/BaseActivity.java
@@ -0,0 +1,40 @@
+package com.auth;
+
+/**
+ * Created by prashant.patel on 11/2/2017.
+ */
+
+import android.app.ProgressDialog;
+import android.support.annotation.VisibleForTesting;
+import android.support.v7.app.AppCompatActivity;
+
+import com.firebaseall.R;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideProgressDialog();
+    }
+
+}
